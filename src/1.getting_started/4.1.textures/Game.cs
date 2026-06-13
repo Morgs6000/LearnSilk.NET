0b57c84d3b07b7e1db4576640a60fcdd8daf66a8
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2026-06-13 11:18:48 -0300

    textures

---
+++ src/1.getting_started/4.1.textures/Game.cs
@@ -0,0 +1,180 @@
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using StbImageSharp;
+
+namespace LearnSilkNET;
+
+public class Game : Engine
+{
+    private ShaderProgram _shader = null!;
+
+    private uint _texture;
+
+    private float[] _vertices =
+    {
+        // positions           // colors           // texture coords
+        -0.5f, -0.5f,  0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
+         0.5f, -0.5f,  0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+         0.5f,  0.5f,  0.0f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
+        -0.5f,  0.5f,  0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f
+    };
+
+    private uint[] _indices =
+    {
+        0, 1, 2, // primeiro triangulo
+        0, 2, 3  // segundo triangulo
+    };
+
+    private uint _vertexArrayObject;
+    private uint _vertexBufferObject;
+    private uint _elementBufferObject;
+
+    // 
+    // --------------------------------------------------
+
+    protected override void OnLoad()
+    {
+        _gl.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        // shader
+        // --------------------------------------------------
+
+        _shader = new ShaderProgram(
+            "shaders/vertex.glsl",
+            "shaders/fragment.glsl"
+        );
+
+        // texture
+        // --------------------------------------------------
+
+        _texture = _gl.GenTexture();
+        _gl.BindTexture(TextureTarget.Texture2D, _texture);
+
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+        byte[] buffer = File.ReadAllBytes("textures/container.jpg");
+        ImageResult image = ImageResult.FromMemory(buffer, ColorComponents.RedGreenBlueAlpha);
+
+        try
+        {
+            unsafe
+            {
+                fixed (byte* ptr = image.Data)
+                {
+                    _gl.TexImage2D(TextureTarget.Texture2D, 0, InternalFormat.Rgba, (uint)image.Width, (uint)image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, ptr);
+                }
+            }
+            _gl.GenerateMipmap(TextureTarget.Texture2D);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(
+                "Falha ao carregar a textura."
+                + "\n\n" + ex
+                + "\n\n" + " -- --------------------------------------------------- -- "
+            );
+        }
+
+        // 
+        // --------------------------------------------------
+
+        _vertexArrayObject = _gl.GenVertexArray();
+        _gl.BindVertexArray(_vertexArrayObject);
+
+        _vertexBufferObject = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vertexBufferObject);
+        unsafe
+        {
+            fixed (float* buf = _vertices)
+            {
+                _gl.BufferData(BufferTargetARB.ArrayBuffer, (uint)(_vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
+            }
+        }
+
+        _elementBufferObject = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _elementBufferObject);
+        unsafe
+        {
+            fixed (uint* buf = _indices)
+            {
+                _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (uint)(_indices.Length * sizeof(uint)), buf, BufferUsageARB.StaticDraw);
+            }
+        }
+
+        // position attribute
+        unsafe
+        {
+            _gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), (void*)0);
+        }
+        _gl.EnableVertexAttribArray(0);
+
+        // color attribute
+        unsafe
+        {
+            _gl.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+        }
+        _gl.EnableVertexAttribArray(1);
+
+        // texture attribute
+        unsafe
+        {
+            _gl.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        }
+        _gl.EnableVertexAttribArray(2);
+
+        // 
+        // --------------------------------------------------
+
+        // _gl.PolygonMode(TriangleFace.FrontAndBack, PolygonMode.Line);
+    }
+
+    protected override void OnResize(Vector2D<int> newSize)
+    {
+        _gl.Viewport(0, 0, (uint)newSize.X, (uint)newSize.Y);
+    }
+
+    protected override void OnUpdate(double deltaTime)
+    {
+        if (Input.GetKey(Key.Escape))
+        {
+            Close();
+        }
+    }
+
+    protected override void OnRender(double deltaTime)
+    {
+        _gl.Clear(ClearBufferMask.ColorBufferBit);
+
+        // shader
+        // --------------------------------------------------
+
+        _shader.Use();
+
+        // texture
+        // --------------------------------------------------
+
+        _gl.BindTexture(TextureTarget.Texture2D, _texture);
+
+        // 
+        // --------------------------------------------------
+
+        _gl.BindVertexArray(_vertexArrayObject);
+
+        unsafe
+        {
+            _gl.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, (void*)0);
+        }
+
+        _gl.BindVertexArray(0);
+    }
+
+    protected override void OnClosing()
+    {
+        
+    }
+}
---
+++ src/1.getting_started/4.1.textures/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnSilkNET;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        new Game().Run();
+    }
+}
