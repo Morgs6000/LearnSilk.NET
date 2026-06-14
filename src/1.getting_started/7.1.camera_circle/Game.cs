8be138461b1bae683f8c3d3a0faca397ea88830c
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2026-06-14 07:39:10 -0300

    camera_circle

---
+++ src/1.getting_started/7.1.camera_circle/Game.cs
@@ -0,0 +1,326 @@
+using System.Numerics;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using StbImageSharp;
+
+namespace LearnSilkNET;
+
+public class Game : Engine
+{
+    private ShaderProgram _shader = null!;
+
+    private uint _texture1;
+    private uint _texture2;
+
+    private float[] _vertices =
+    {
+        // positions           // colors           // texture coords
+        -0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,         // 0
+        -0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,         // 1
+        -0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,         // 2
+        -0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,         // 3
+
+         0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,         // 4
+         0.5f, -0.5f, -0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,         // 5
+         0.5f,  0.5f, -0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,         // 6
+         0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,         // 7
+
+        -0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,         // 8
+         0.5f, -0.5f, -0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,         // 9
+         0.5f, -0.5f,  0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,         // 10
+        -0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,         // 11
+
+        -0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,         // 12
+         0.5f,  0.5f,  0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,         // 13
+         0.5f,  0.5f, -0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,         // 14
+        -0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,         // 15
+
+         0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,         // 16
+        -0.5f, -0.5f, -0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,         // 17
+        -0.5f,  0.5f, -0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,         // 18
+         0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,         // 19
+
+        -0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,         // 20
+         0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,         // 21
+         0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,         // 22
+        -0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f          // 23
+    };
+
+    private uint[] _indices =
+    {
+         0,  1,  2, // primeiro triangulo
+         0,  2,  3, // segundo triangulo
+
+         4,  5,  6, // primeiro triangulo
+         4,  6,  7, // segundo triangulo
+
+         8,  9, 10, // primeiro triangulo
+         8, 10, 11, // segundo triangulo
+
+        12, 13, 14, // primeiro triangulo
+        12, 14, 15, // segundo triangulo
+
+        16, 17, 18, // primeiro triangulo
+        16, 18, 19, // segundo triangulo
+
+        20, 21, 22, // primeiro triangulo
+        20, 22, 23  // segundo triangulo
+    };
+
+    private uint _vertexArrayObject;
+    private uint _vertexBufferObject;
+    private uint _elementBufferObject;
+
+    private Vector3[] cubePositions =
+    {
+        new Vector3( 0.0f,  0.0f,  0.0f),
+        new Vector3( 2.0f,  5.0f, -15.0f),
+        new Vector3(-1.5f, -2.2f, -2.5f),
+        new Vector3(-3.8f, -2.0f, -12.3f),
+        new Vector3( 2.4f, -0.4f, -3.5f),
+        new Vector3(-1.7f,  3.0f, -7.5f),
+        new Vector3( 1.3f, -2.0f, -2.5f),
+        new Vector3( 1.5f,  2.0f, -2.5f),
+        new Vector3( 1.5f,  0.2f, -1.5f),
+        new Vector3(-1.3f,  1.0f, -1.5f)
+    };
+
+    // 
+    // --------------------------------------------------
+
+    protected override void OnLoad()
+    {
+        _gl.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        // shader
+        // --------------------------------------------------
+
+        _shader = new ShaderProgram(
+            "shaders/vertex.glsl",
+            "shaders/fragment.glsl"
+        );
+
+        // texture 1
+        // --------------------------------------------------
+
+        _texture1 = _gl.GenTexture();
+        _gl.BindTexture(TextureTarget.Texture2D, _texture1);
+
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+        StbImage.stbi_set_flip_vertically_on_load(1);
+
+        byte[] buffer = File.ReadAllBytes("textures/container.jpg");
+        ImageResult image = ImageResult.FromMemory(buffer, ColorComponents.RedGreenBlueAlpha);
+
+        try
+        {
+            unsafe
+            {
+                fixed (byte* ptr = image.Data)
+                {
+                    _gl.TexImage2D(TextureTarget.Texture2D, 0, InternalFormat.Rgba, (uint)image.Width, (uint)image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, ptr);
+                }
+            }
+            _gl.GenerateMipmap(TextureTarget.Texture2D);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(
+                "Falha ao carregar a textura."
+                + "\n\n" + ex
+                + "\n\n" + " -- --------------------------------------------------- -- "
+            );
+        }
+
+        // texture 2
+        // --------------------------------------------------
+
+        _texture2 = _gl.GenTexture();
+        _gl.BindTexture(TextureTarget.Texture2D, _texture2);
+
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
+        _gl.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+        buffer = File.ReadAllBytes("textures/awesomeface.png");
+        image = ImageResult.FromMemory(buffer, ColorComponents.RedGreenBlueAlpha);
+
+        try
+        {
+            unsafe
+            {
+                fixed (byte* ptr = image.Data)
+                {
+                    _gl.TexImage2D(TextureTarget.Texture2D, 0, InternalFormat.Rgba, (uint)image.Width, (uint)image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, ptr);
+                }
+            }
+            _gl.GenerateMipmap(TextureTarget.Texture2D);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(
+                "Falha ao carregar a textura."
+                + "\n\n" + ex
+                + "\n\n" + " -- --------------------------------------------------- -- "
+            );
+        }
+
+        // 
+        // --------------------------------------------------
+
+        _vertexArrayObject = _gl.GenVertexArray();
+        _gl.BindVertexArray(_vertexArrayObject);
+
+        _vertexBufferObject = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vertexBufferObject);
+        unsafe
+        {
+            fixed (float* buf = _vertices)
+            {
+                _gl.BufferData(BufferTargetARB.ArrayBuffer, (uint)(_vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
+            }
+        }
+
+        _elementBufferObject = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _elementBufferObject);
+        unsafe
+        {
+            fixed (uint* buf = _indices)
+            {
+                _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (uint)(_indices.Length * sizeof(uint)), buf, BufferUsageARB.StaticDraw);
+            }
+        }
+
+        // position attribute
+        unsafe
+        {
+            _gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), (void*)0);
+        }
+        _gl.EnableVertexAttribArray(0);
+
+        // color attribute
+        unsafe
+        {
+            _gl.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+        }
+        _gl.EnableVertexAttribArray(1);
+
+        // texture attribute
+        unsafe
+        {
+            _gl.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        }
+        _gl.EnableVertexAttribArray(2);
+
+        // 
+        // --------------------------------------------------
+
+        // _gl.PolygonMode(TriangleFace.FrontAndBack, PolygonMode.Line);
+
+        _gl.Enable(EnableCap.DepthTest);
+    }
+
+    protected override void OnResize(Vector2D<int> newSize)
+    {
+        _gl.Viewport(0, 0, (uint)newSize.X, (uint)newSize.Y);
+    }
+
+    protected override void OnUpdate(double deltaTime)
+    {
+        if (Input.GetKey(Key.Escape))
+        {
+            Close();
+        }
+    }
+
+    protected override void OnRender(double deltaTime)
+    {
+        _gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+        // shader
+        // --------------------------------------------------
+
+        _shader.Use();
+
+        _shader.SetInt("texture1", 0);
+        _shader.SetInt("texture2", 1);
+
+        Matrix4x4 model = Matrix4x4.Identity;
+
+        Matrix4x4 view = Matrix4x4.Identity;
+
+        const float radius = 10.0f;
+        float camX = MathF.Sin(Time.ElapsedTime) * radius;
+        float camZ = MathF.Cos(Time.ElapsedTime) * radius;
+
+        view *= Matrix4x4.CreateLookAt(
+            cameraPosition: new Vector3(camX, 0.0f, camZ),
+            cameraTarget:   new Vector3(0.0f, 0.0f, 0.0f),
+            cameraUpVector: new Vector3(0.0f, 1.0f, 0.0f)
+        );
+
+        Matrix4x4 projection = Matrix4x4.Identity;
+        projection *= Matrix4x4.CreatePerspectiveFieldOfView(
+            fieldOfView:       Mathf.Radians(45.0f),
+            aspectRatio:       (float)Screen.Widht / (float)Screen.Height,
+            nearPlaneDistance: 0.1f,
+            farPlaneDistance:  100.0f
+        );
+
+        _shader.SetMatrix4x4("model", model);
+        _shader.SetMatrix4x4("view", view);
+        _shader.SetMatrix4x4("projection", projection);
+
+        // texture 1
+        // --------------------------------------------------
+
+        _gl.ActiveTexture(TextureUnit.Texture0);
+        _gl.BindTexture(TextureTarget.Texture2D, _texture1);
+
+        // texture 2
+        // --------------------------------------------------
+
+        _gl.ActiveTexture(TextureUnit.Texture1);
+        _gl.BindTexture(TextureTarget.Texture2D, _texture2);
+
+        // 
+        // --------------------------------------------------
+
+        _gl.BindVertexArray(_vertexArrayObject);
+
+        for (int i = 0; i < 10; i++)
+        {
+            model = Matrix4x4.Identity;
+
+            float angle = 20.0f * i;
+            model *= Matrix4x4.CreateFromAxisAngle(
+                Vector3.Normalize(new Vector3(1.0f, 0.3f, 0.5f)),
+                Mathf.Radians(angle)
+            );
+
+            model *= Matrix4x4.CreateTranslation(cubePositions[i]);
+
+            _shader.SetMatrix4x4("model", model);
+
+            unsafe
+            {
+                _gl.DrawElements(PrimitiveType.Triangles, (uint)_indices.Length, DrawElementsType.UnsignedInt, (void*)0);
+            }
+        }        
+
+        _gl.BindVertexArray(0);
+    }
+
+    protected override void OnClosing()
+    {
+        
+    }
+}
